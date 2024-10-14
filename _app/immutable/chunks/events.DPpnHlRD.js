9cee7e31e4b2499007874e150a6c55c55ee1e922
Oviedo Lab <oviedolab.cuny@gmail.com>
2024-10-14 17:11:16 -0500

    Added Francesca and partial controller support

---
+++ _app/immutable/chunks/events.DPpnHlRD.js
@@ -0,0 +1,1 @@
+import{A as a}from"./scheduler.DqcNTmzW.js";function i(){const n=a();return t=>{const{originalEvent:e}=t.detail,{cancelable:r}=t,c=e.type;n(c,{originalEvent:e,currentTarget:e.currentTarget},{cancelable:r})||t.preventDefault()}}export{i as c};
---
+++ _app/immutable/entry/start.whCamnwe.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.ckO3O-_t.js";export{t as start};
