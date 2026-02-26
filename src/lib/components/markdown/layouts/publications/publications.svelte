b99b57d4175ca75adddf5af077947068a8a48508
codynhanpham <87593612+codynhanpham@users.noreply.github.com>
2026-02-26 15:09:53 -0600

    Remove current build

--- src/lib/components/markdown/layouts/publications/publications.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    let { children } = $props();
-</script>
-
-
-<div data-slot="markdown-content">
-    {@render children?.()}
-</div>
--- src/lib/utils/color.ts
+++
@@ -1,5 +0,0 @@
-import {
-    getRandomAAColor,
-} from 'accessible-colors';
-
-// TODO: Dynamic accent color generation that take into account the background + text colors. Also somehow handle theme changes
